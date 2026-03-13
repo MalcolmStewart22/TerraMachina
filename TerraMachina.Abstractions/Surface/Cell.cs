93e68b6f5f174a049145d9a959f56397c5c3d84a
Malcolm Stewart <malcolm.stewart22@gmail.com>
2026-03-13 14:13:40 -0600

    refactor(abstractions): clean up cell data model

--- TerraMachina.Abstractions/Surface/Cell.cs
+++ TerraMachina.Abstractions/Surface/Cell.cs
@@ -10,28 +10,28 @@ public class Cell
     public int CellId { get; init; }
     public Vector3 Position { get; init; }
     public int[] NeighborsById { get; init; } = new int[3];
+    public bool IsOcean { get; set; }
     public BiomeType Biome { get; set; }
     //Resources
     public Dictionary<GeologicalResource, ResourceDeposit>? GeologicalResources { get; set; } //Resource -> Quantity, Quality
     public Dictionary<FaunaResource, ResourceDeposit>? FaunaResources { get; set; } //Resource -> Quantity, Quality
     public Dictionary<FloraResource, ResourceDeposit>? FloraResources { get; set; } //Resource -> Quantity, Quality
     //Geology Data
-    public int Elevation { get; set; }
-    public SoilType Soil { get; set; }
-    public BedrockType Bedrock { get; set; }
+    public int Elevation { get; init; }
+    public SoilType Soil { get; init; }
+    public float CurrentFertility { get; set; }
+    public BedrockType Bedrock { get; init; }
     //Hydrology Data
-    public bool HasRiver { get; set; }
     public int? RiverId { get; set; }
-    public bool IsLake { get; set; }
     public int? LakeId { get; set; }
     public float SoilWaterContent { get; set; }
+    ///<summary> Measured in Meters</summary>
     public int StandingWater { get; set; }
+    ///<summary> Measured in Meters</summary>
     public int SnowPack { get; set; }
     public bool IsWetland { get; set; }
     public bool IsCoastland { get; set; }
     //Atmosphere Data
     public float Temperature { get; set; }
     public float Humidty { get; set; }
-    public Vector2 WindDirection { get; set; }
-    public float WindSpeed { get; set; }
 }
