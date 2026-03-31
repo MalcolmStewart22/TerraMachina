22e7d653702d6be6524ed7c62d3081a62f8869ce
Malcolm Stewart <malcolm.stewart22@gmail.com>
2026-03-31 15:01:59 -0600

    chore(abstraction) moved payload objects into separate folders based on stage.

--- src/TerraMachina.Abstractions/ProgressUpdate/WorldGen/Geometry/CellGeometryMinimum.cs
+++ src/TerraMachina.Abstractions/ProgressUpdate/WorldGen/Geometry/CellGeometryMinimum.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Numerics;
 using System.Text;
 
-namespace TerraMachina.Abstractions.ProgressUpdate.WorldGen;
+namespace TerraMachina.Abstractions.ProgressUpdate.WorldGen.Geometry;
 
 public  class CellGeometryMinimum
 {
--- src/TerraMachina.Abstractions/ProgressUpdate/WorldGen/Geometry/GeometryProgressPayload.cs
+++ src/TerraMachina.Abstractions/ProgressUpdate/WorldGen/Geometry/GeometryProgressPayload.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace TerraMachina.Abstractions.ProgressUpdate.WorldGen;
+namespace TerraMachina.Abstractions.ProgressUpdate.WorldGen.Geometry;
 
 public class GeometryProgressPayload : IWorldGenPayload
 {
