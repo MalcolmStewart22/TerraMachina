7757e36bc6472a086d5cad65fa914f4da89dae55
Malcolm Stewart <malcolm.stewart22@gmail.com>
2026-04-29 10:45:33 -0600

    fix(runtime) fix CORS setup for prod

--- src/TerraMachina.Runtime/Program.cs
+++ src/TerraMachina.Runtime/Program.cs
@@ -5,19 +5,23 @@ using TerraMachina.Runtime.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSignalR(options =>
 {
     options.MaximumReceiveMessageSize = null; // no limit
     options.StreamBufferCapacity = 20;
+})
+.AddJsonProtocol(options =>
+{
+    options.PayloadSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    options.PayloadSerializerOptions.Converters.Add(new Vector3JsonConverter());
 });
 builder.Services.AddSingleton<IEngineService, EngineService>();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("DevClient", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("https://terramachina.studionex.dev", "http://localhost:5173")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
@@ -28,12 +32,6 @@ builder.Services.AddControllers()
     {
         options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
         options.JsonSerializerOptions.Converters.Add(new Vector3JsonConverter());
-    });
-builder.Services.AddSignalR()
-    .AddJsonProtocol(options =>
-    {
-        options.PayloadSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
-        options.PayloadSerializerOptions.Converters.Add(new Vector3JsonConverter());
     });
 
 var app = builder.Build();
